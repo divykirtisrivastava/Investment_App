3b3e23f410d2090072b4227462b6d745b5a0cb45
divykirtisrivastava <divykirtisrivastava@gmail.com>
2024-10-03 16:43:51 +0530

    added

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import { Navbar } from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import logo from '../../public/logo.png'
+import Head from "next/head";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,12 +27,15 @@ export default function RootLayout({
       
   return (
     <html lang="en">
+      
       <body className={`${inter.className} dark`}>
         {children}
-        
-    <script src="https://cdn.botpress.cloud/webchat/v2.2/inject.js"></script>
-<script src="https://files.bpcontent.cloud/2024/10/03/10/20241003105514-1YWN4TX3.js"></script>
-    
+        <Script
+          src="https://cdn.botpress.cloud/webchat/v2.2/inject.js"
+        />
+        <Script
+          src="https://files.bpcontent.cloud/2024/10/03/10/20241003105514-1YWN4TX3.js"
+        />
         </body>
     </html>
   );
