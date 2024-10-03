783f90a51956d23847a3357d060c52b31a7034c4
divykirtisrivastava <divykirtisrivastava@gmail.com>
2024-10-03 16:29:31 +0530

    added

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -20,10 +20,17 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  
+ 
+      
   return (
     <html lang="en">
       <body className={`${inter.className} dark`}>
         {children}
+        
+    <script src="https://cdn.botpress.cloud/webchat/v2.2/inject.js"></script>
+<script src="https://files.bpcontent.cloud/2024/10/03/10/20241003105514-1YWN4TX3.js"></script>
+    
         </body>
     </html>
   );
